7de451227b164ade827f8dcdd8f6185d2df85644
ericchentch <ericchen.tch@gmail.com>
2023-02-23 00:13:31 +0700

    restructure

--- src/backend/app/app.controller.ts
+++ src/backend/app/app.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common'
 
 @Controller('randomNumber')
 export class AppController {
   @Get()
-  randomNumber() {
-    return Math.random() * 100;
+  async randomNumber() {
+    return Math.random() * 100
   }
 
   @Get('/:number')
   async findOne(@Param('number') param: string) {
-    return { param }; // You don't even need a return, I put it just to have some return.
+    return { param } // You don't even need a return, I put it just to have some return.
   }
 }
