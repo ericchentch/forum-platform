14d18ab295fd2e4078597ca9e246eb8a2e7b1f69
ericchentch <ericchen.tch@gmail.com>
2023-02-22 23:52:58 +0700

    init

---
+++ src/client/pages/api/[...catchAll].ts
@@ -0,0 +1,10 @@
+import { Backend } from '@/backend/main';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextApiRequest, res: NextApiResponse) =>
+  new Promise(async (resolve) => {
+    const listener = await Backend.getListener();
+    listener(req, res);
+    res.on('finish', resolve);
+  });
